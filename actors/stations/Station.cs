975bf46ff4ed9aa7092ab4a17441091077c25cee
Quadtree <quadtree@gmail.com>
2022-10-01 12:19:03 -0700

    U

---
+++ actors/stations/Station.cs
@@ -0,0 +1,7 @@
+using Godot;
+
+public class Station : Spatial
+{
+    [Export]
+    Recipe.Ing IngredientDelivered;
+}
---
+++ gs/Recipe.cs
@@ -0,0 +1,8 @@
+public class Recipe
+{
+    public enum Ing
+    {
+        None,
+        Lettuce,
+    }
+}
