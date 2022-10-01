f84c908cdb844498caf404457e9cdad252cf1cdc
Quadtree <quadtree@gmail.com>
2022-10-01 13:03:01 -0700

    U

--- actors/Cart.cs
+++ actors/Cart.cs
@@ -1,5 +1,9 @@
-public class Cart
+using Godot;
+
+public class Cart : Spatial
 {
     // carts follow a series of steps
-    // normally 
+    // carts take 1 move per 0.5 seconds
+
+    const float CART_MOVE_TIME = 0.5f;
 }
