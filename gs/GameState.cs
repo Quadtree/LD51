fa74ffd01ac9f3260a1b66927f9016deb9ed07b0
Quadtree <quadtree@gmail.com>
2022-10-01 13:35:44 -0700

    U

--- gs/GameState.cs
+++ gs/GameState.cs
@@ -1,9 +1,18 @@
-using System.Runtime.InteropServices;
-using Godot.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class GameState
 {
     public Dictionary<int, CartState> CartStates;
 
     public Dictionary<int, StationState> StationStates;
+
+    public GameState Clone()
+    {
+        return new GameState
+        {
+            CartStates = CartStates.ToDictionary(it => it.Key, it => it.Value),
+            StationStates = StationStates.ToDictionary(it => it.Key, it => it.Value),
+        };
+    }
 }
