a8a30e82befa163a00303b99b9544164c74e9ae7
Quadtree <quadtree@gmail.com>
2022-10-02 13:56:46 -0700

    U

---
+++ actors/RepeatingSoundEffect.cs
@@ -0,0 +1,19 @@
+using System;
+using Godot;
+
+public class RepeatingSoundEffect : Node
+{
+    public float TimeLeft = 0;
+    public string Path;
+
+    public override void _Ready()
+    {
+
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+
+    }
+}
