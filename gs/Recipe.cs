88a9be23b1f1b82008b5b3f9c1e51a6ee15e5508
Quadtree <quadtree@gmail.com>
2022-10-01 22:33:16 -0700

    U

--- gs/Recipe.cs
+++ gs/Recipe.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 public class Recipe
 {
     public enum Ing
@@ -13,9 +15,31 @@ public class Recipe
     }
 
     public Ing[] Ings;
+
+    public Recipe(params Ing[] ings)
+    {
+        this.Ings = ings;
+    }
+
+    private string _Name;
+
+    public string Name
+    {
+        get
+        {
+
+            if (_Name == null)
+            {
+                var rx = new Regex("([a-z])([A-Z])");
+                _Name = rx.Replace(GetType().Name, (it) => it.Groups[1].Value + " " + it.Groups[2].Value);
+            }
+            return _Name;
+        }
+    }
 }
 
 public class Recipes
 {
-    public static Recipe
+    public static Recipe SimpleSalad = new Recipe(Recipe.Ing.Lettuce);
+    public static Recipe ChoppedSalad = new Recipe(Recipe.Ing.Lettuce);
 }
