7178f7bc23e7e52cc8ffeacc64ef6fa305acb29b
Quadtree <quadtree@gmail.com>
2022-10-02 01:07:06 -0700

    U

--- actors/UpcomingRecipe.cs
+++ actors/UpcomingRecipe.cs
@@ -12,7 +12,7 @@ public class UpcomingRecipe : Spatial
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-
+        this.FindChildByType<Label>().Text = $"{Recipe.Name}";
     }
 
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
