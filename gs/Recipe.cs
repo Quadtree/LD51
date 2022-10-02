b74b0de17085228d7fb599b823d1ba45e554f3ac
Quadtree <quadtree@gmail.com>
2022-10-02 01:09:46 -0700

    U

--- gs/Recipe.cs
+++ gs/Recipe.cs
@@ -16,37 +16,23 @@ public class Recipe
     }
 
     public Ing[] Ings;
+    public string Name;
 
-    public Recipe(params Ing[] ings)
+    public Recipe(string name, params Ing[] ings)
     {
         this.Ings = ings;
-    }
-
-    private string _Name;
-
-    public string Name
-    {
-        get
-        {
-
-            if (_Name == null)
-            {
-                var rx = new Regex("([a-z])([A-Z])");
-                _Name = rx.Replace(GetType().Name, (it) => it.Groups[1].Value + " " + it.Groups[2].Value);
-            }
-            return _Name;
-        }
+        this.Name = name;
     }
 }
 
 public class Recipes
 {
-    public static Recipe SimpleSalad = new Recipe(Recipe.Ing.Lettuce);
-    public static Recipe ChoppedSalad = new Recipe(Recipe.Ing.Lettuce, Recipe.Ing.Chop);
-    public static Recipe MixedSalad = new Recipe(Recipe.Ing.Lettuce, Recipe.Ing.Tomato, Recipe.Ing.Chop);
-    public static Recipe Soup = new Recipe(Recipe.Ing.Water, Recipe.Ing.Tomato, Recipe.Ing.Cook);
-    public static Recipe Sandwich = new Recipe(Recipe.Ing.Bread, Recipe.Ing.Tomato, Recipe.Ing.Protein);
-    public static Recipe ToastedSandwich = new Recipe(Recipe.Ing.Bread, Recipe.Ing.Tomato, Recipe.Ing.Protein, Recipe.Ing.Cook);
+    public static Recipe SimpleSalad = new Recipe("Simple Salad", Recipe.Ing.Lettuce);
+    public static Recipe ChoppedSalad = new Recipe("Chopped Salad", Recipe.Ing.Lettuce, Recipe.Ing.Chop);
+    public static Recipe MixedSalad = new Recipe("Mixed Salad", Recipe.Ing.Lettuce, Recipe.Ing.Tomato, Recipe.Ing.Chop);
+    public static Recipe Soup = new Recipe("Soup", Recipe.Ing.Water, Recipe.Ing.Tomato, Recipe.Ing.Cook);
+    public static Recipe Sandwich = new Recipe("Sandwich", Recipe.Ing.Bread, Recipe.Ing.Tomato, Recipe.Ing.Protein);
+    public static Recipe ToastedSandwich = new Recipe("Toasted Sandwich", Recipe.Ing.Bread, Recipe.Ing.Tomato, Recipe.Ing.Protein, Recipe.Ing.Cook);
 }
 
 public struct IngModel
