b7553705c8dd04ed61978e1bcb6f35b53c8bc98a
Quadtree <quadtree@gmail.com>
2022-10-02 00:44:35 -0700

    U

--- gs/Recipe.cs
+++ gs/Recipe.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using Godot.Collections;
 
 public class Recipe
 {
@@ -46,4 +47,25 @@ public class Recipes
     public static Recipe Soup = new Recipe(Recipe.Ing.Water, Recipe.Ing.Tomato, Recipe.Ing.Cook);
     public static Recipe Sandwich = new Recipe(Recipe.Ing.Bread, Recipe.Ing.Tomato, Recipe.Ing.Protein);
     public static Recipe ToastedSandwich = new Recipe(Recipe.Ing.Bread, Recipe.Ing.Tomato, Recipe.Ing.Protein, Recipe.Ing.Cook);
+}
+
+public struct IngModel
+{
+    public string Path;
+    public float Height;
+}
+
+public static class IngModels
+{
+    public static Dictionary<Recipe.Ing, IngModel> Data;
+
+    static IngModels()
+    {
+        Data = new Dictionary<Recipe.Ing, IngModel>();
+        Data[Recipe.Ing.Lettuce] = new IngModel { Path = "res://models/lettuce.glb", Height = 0.2f };
+        Data[Recipe.Ing.Protein] = new IngModel { Path = "res://models/protein.glb", Height = 0.2f };
+        Data[Recipe.Ing.Bread] = new IngModel { Path = "res://models/bread.glb", Height = 0.3f };
+        Data[Recipe.Ing.Tomato] = new IngModel { Path = "res://models/tomato.glb", Height = 0.4f };
+        Data[Recipe.Ing.Water] = new IngModel { Path = "res://models/water.glb", Height = 0.8f };
+    }
 }
