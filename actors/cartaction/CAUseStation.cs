c849e2c1868367dc3452fc451c8ff81d18321c15
Quadtree <quadtree@gmail.com>
2022-10-02 13:55:57 -0700

    U

--- actors/cartaction/CAUseStation.cs
+++ actors/cartaction/CAUseStation.cs
@@ -5,6 +5,12 @@ public class CAUseStation : CartAction
     public override void Execute(IMutableGameState state, bool printDebugData)
     {
         state.GetStationState(StationID).UseOn(state, CartID, printDebugData);
+
+        if (printDebugData){
+            if (state.GetStationState(StationID).Ing == Recipe.Ing.Cook){
+                
+            }
+        }
     }
 
     public override string ToString()
